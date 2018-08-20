ce1474026934449a70efd71ef71dc57872ec8b1d
Abhijit <abangera@juniper.net>
2018-08-20 18:11:51 +0530

    queue implementation

---
+++ queue.py
@@ -0,0 +1,34 @@
+
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    def isEmpty(self):
+        return self.queue == []
+
+    def enqueue(self, data):
+        self.queue.append(data)
+
+    def dequeue(self):
+        data = self.queue[0]
+        del self.queue[0]
+        return data
+
+    def peek(self):
+        return self.queue[0]
+
+    def sizeQueue(self):
+        return len(self.queue)
+
+
+queue = Queue()
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+print queue.sizeQueue()
+print queue.dequeue()
+print queue.peek()
+print queue.dequeue()
+print queue.peek()
